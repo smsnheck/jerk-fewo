2444b10efcd12e7efa12326e14200534dcf390e3
Sebastian Heckmann <sebastian.heckmann@googlemail.com>
2015-06-21 16:51:26 +0200

    updates the naming from Ferienwohnung to Ferienhaus, adds the correct address to the gmap

--- app/scripts/controllers/locationCtrl.js
+++ app/scripts/controllers/locationCtrl.js
@@ -3,5 +3,6 @@ var appControllers = angular.module('jerk.fewo.location', ['uiGmapgoogle-maps'])
 
 
 appControllers.controller('LocationCtrl', function($scope){
-$scope.map = { center: { latitude: 53.456603, longitude: 5.798587}, zoom: 14 };
+$scope.map = { center: { latitude: 53.456761, longitude: 5.798592}, zoom: 14 };
+$scope.marker = { id: 0, coords: { latitude: 53.456761, longitude: 5.798592}, options: {labelContent: 'Jerk Ferienhaus'}};
 });
